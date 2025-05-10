8e404aa94a6aa624bdca013cc965968256db7e29
davirfs <davirfs@hotmail.com>
2025-05-09 23:21:03 -0300

    cria uma entidade de banco de dados

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/domain/entity/Ninja.java
@@ -0,0 +1,74 @@
+package dev.java10x.CadastroDeNinjas.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class Ninja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private Integer idade;
+
+    public Ninja(String nome, String email, Integer idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public Ninja() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ninja ninja = (Ninja) o;
+        return Objects.equals(nome, ninja.nome) && Objects.equals(email, ninja.email) && Objects.equals(idade, ninja.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, email, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Ninja{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
